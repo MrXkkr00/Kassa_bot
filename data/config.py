c2a3f231acae461cecf73a28d505810ffda26a1f
RasulbekQadomboyev <rasqadomboyev@gmail.com>
2024-09-03 12:24:44 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,14 +1,10 @@
-# BOT_TOKEN = "6778545348:AAEsZTdDDUg8g8y1RH8fYNYntpWcRFjKuH8"
-BOT_TOKEN = "6594381526:AAErdHaZzOj8-NqQqjMdvjhnhxW7d1c0m0I"
-ADMINS = ["984568970"]
+
+BOT_TOKEN = ""
+ADMINS = [""]
 IP = "localhost"
 
-kassirlar = {"170517008": 'Ulugbek Qurbonov', "5255919813": 'Dovud Ilhomov',
-             "82237568": 'Akmal Xushanov', "1717400120": 'Bobomurod Sirojiddinov', 
-             "633430527": 'Fazliddin Jumayev', "495973790": 'Ramzbek Qurbonov', 
-             "6106677414": 'Mirjalol Qurbonov', "984568970": "Rasulbek",
-             "1088844386": "Shoxrux", "6003648169": "Usmonjon Nematov"}
-admin_user_id = 5255919813
+kassirlar = {"984568970": "Rasulbek"}
+admin_user_id = 
 
 # DB_USER = "postgres"
 # DB_PASS = "12345ras"
@@ -16,7 +12,3 @@ admin_user_id = 5255919813
 # DB_HOST = "localhost"
 
 
-# kassirlar = {"170517008": 'Ulugbek Qurbonov',
-#              "5255919813": 'Bobomurod Sirojiddinov',
-#              "633430527": 'Fazliddin Jumayev', "495973790": 'Ramzbek Qurbonov', "6106677414": 'Mirjalol Qurbonov',
-#              "984568970": "Rasulbek"}
